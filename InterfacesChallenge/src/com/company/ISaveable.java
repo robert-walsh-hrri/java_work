62012f074b9c21fa6d79fa93e23c1f69277ea198
robert-walsh-hrri <rwalsh@gmail.com>
2020-10-30 14:56:57 -0500

    Interface challenge done

---
+++ InterfacesChallenge/src/com/company/ISaveable.java
@@ -0,0 +1,8 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public interface ISaveable {
+    public ArrayList<String> saveValues();
+    public void populateValues(ArrayList<String> values);
+}
