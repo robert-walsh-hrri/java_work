62b7a3b5d86457976a241fe4eece32c1997fbf96
robert-walsh-hrri <rwalsh@gmail.com>
2020-11-01 10:16:11 -0600

    Webserver working

--- Webserver/src/com/company/Main.java
+++ Webserver/src/com/company/Main.java
@@ -9,22 +9,26 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
-import java.util.logging.Logger;
+// import java.util.logging.Logger;
 
-//import org.apache.commons.lang3.StringEscapeUtils;
+// import org.apache.commons.lang3.StringEscapeUtils;
 
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Logger logger = Logger.getLogger("httpserver");
+        // Logger logger = Logger.getLogger("httpserver");
+
         HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 8001), 0);
 
         ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
 
         server.createContext("/test", new MyHttpHandler());
+
         server.setExecutor(threadPoolExecutor);
+
         server.start();
+
         System.out.println(" Server started on port 8001");
     }
 
@@ -33,41 +37,56 @@ public class Main {
         @Override
         public void handle(HttpExchange httpExchange) throws IOException {
             String requestParamValue=null;
-            if("GET".equals(httpExchange.getRequestMethod())) {
+
+            if ("GET".equals(httpExchange.getRequestMethod())) {
+
                 requestParamValue = handleGetRequest(httpExchange);
+
             }else if("POST".equals(httpExchange)) {
-                //requestParamValue = handlePostRequest(httpExchange);
+                // write post method before uncommenting
+                // requestParamValue = handlePostRequest(httpExchange);
             }
+
             handleResponse(httpExchange,requestParamValue);
         }
 
         private String handleGetRequest(HttpExchange httpExchange) {
-            return httpExchange.
-            getRequestURI()
+
+            return httpExchange
+                    .getRequestURI()
                     .toString()
                     .split("\\?")[1]
                     .split("=")[1];
         }
 
         private void handleResponse(HttpExchange httpExchange, String requestParamValue)  throws  IOException {
+
             OutputStream outputStream = httpExchange.getResponseBody();
+
             StringBuilder htmlBuilder = new StringBuilder();
 
-            htmlBuilder.append("<html>").
-            append("<body>").
-            append("<h1>").
-            append("Hello ")
-                    .append(requestParamValue)
-                    .append("</h1>")
-                    .append("</body>")
-                    .append("</html>");
+            htmlBuilder.append("<html>")
+                        .append("<body>")
+                        .append("<h1>")
+                        .append("Hello ")
+                        .append(requestParamValue)
+                        .append("</h1>")
+                        .append("</body>")
+                        .append("</html>");
+
             // encode HTML content
-            String htmlResponse = StringEscapeUtils.escapeHtml4(htmlBuilder.toString());
+            // below does not seem to be necessary
+            // String htmlResponse = StringEscapeUtils.escapeHtml4(htmlBuilder.toString());
+
+            String htmlResponse = htmlBuilder.toString();
 
             // this line is a must
+
             httpExchange.sendResponseHeaders(200, htmlResponse.length());
             outputStream.write(htmlResponse.getBytes());
+
             outputStream.flush();
+
             outputStream.close();
         }
     }
